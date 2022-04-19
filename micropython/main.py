8465133f2ea40a068be368c8b294dae299a68cf2
r15hil <rishilapple@gmail.com>
2022-04-19 11:50:16 +0100

    data

--- micropython/main.py
+++ micropython/main.py
@@ -22,8 +22,11 @@ DEFAULT_SPI_PARAMS = {
     "sck": 18,
     "mosi": 23,
     "miso": 19,
-    "output_amp_gain": 255,  # value between 0-255 controlling gain of output amplifier
+    "output_amp_gain": 50,  # value between 0-255 controlling gain of output amplifier
 }
+
+get_param = lambda key: Pin(DEFAULT_SPI_PARAMS[key])
+temp_spi_params = {key: get_param(key) for key in ["sck", "miso", "mosi"]}
 
 spi = SPI(
     2,
@@ -39,3 +42,33 @@ spi = SPI(
 data = bytearray(DEFAULT_SPI_PARAMS['output_amp_gain'])
 # data = bytearray(100)
 spi.write(data)
+
+ssid = 'TP-Link_AP_4C04'
+password = '63525465'
+connect_wifi(ssid, password)
+
+###############################################################################
+
+adc_sample = []
+pot_size = 512
+
+def sample_callback(*args, **kwargs):
+    global adc_sample
+    if len(adc_sample) > pot_size:
+        del adc_sample[0]
+        adc_sample.append(adc.read())
+    else:
+        adc_sample.append(adc.read())
+
+sample_timer = Timer(0)
+sample_timer.init(freq=pot_size, callback=sample_callback)
+
+for i in range(30):
+    time.sleep(1)
+    print(gc.mem_free())
+    data = adc_sample
+    toSend = {"raw_data":data}
+    print(toSend)
+    requests.JSONRequest("http://192.168.0.37:5001/collect", toSend)
+
+requests.GETRequest("http://192.168.0.37:5001/save")
