ee12acd2ffac4d2e94ddb5f6d14f3f26dd5f1a6f
r15hil <rishilapple@gmail.com>
2022-04-20 19:52:18 +0100

    with data from disconnected test

--- micropython/main.py
+++ micropython/main.py
@@ -22,7 +22,7 @@ DEFAULT_SPI_PARAMS = {
     "sck": 18,
     "mosi": 23,
     "miso": 19,
-    "output_amp_gain": 100,  # value between 0-255 controlling gain of output amplifier
+    "output_amp_gain": 200,  # value between 0-255 controlling gain of output amplifier
 }
 
 get_param = lambda key: Pin(DEFAULT_SPI_PARAMS[key])
@@ -38,11 +38,20 @@ spi = SPI(
     miso=Pin(19),
 )
 
-# data = bytearray([17, DEFAULT_SPI_PARAMS['output_amp_gain']])
-data = bytearray(DEFAULT_SPI_PARAMS['output_amp_gain'])
-# data = bytearray(100)
-spi.write(data)
+# setting gain #
 
+# data = bytearray([17, DEFAULT_SPI_PARAMS['output_amp_gain']])
+data = bytearray([17,DEFAULT_SPI_PARAMS['output_amp_gain']])
+# data = bytearray(100)
+cs = machine.Pin(5, machine.Pin.OUT)
+
+# have to pull GPIO 5 LOW before writing the gain
+
+cs.on() #active low
+spi.write(data)
+cs.off() #active high
+
+# will change lol
 ssid = 'TP-Link_AP_4C04'
 password = '63525465'
 connect_wifi(ssid, password)
@@ -50,11 +59,14 @@ connect_wifi(ssid, password)
 ###############################################################################
 
 adc_sample = []
+
+# set for frequency the adc should be read and set the size of the array to send every t seconds 
 pot_size = 512
 
 def sample_callback(*args, **kwargs):
     global adc_sample
     if len(adc_sample) > pot_size:
+        # fifo
         del adc_sample[0]
         adc_sample.append(adc.read())
     else:
@@ -64,6 +76,7 @@ sample_timer = Timer(0)
 sample_timer.init(freq=pot_size, callback=sample_callback)
 
 for i in range(30):
+    # set send time
     time.sleep(1)
     print(gc.mem_free())
     data = adc_sample
