06760f36a61914ecef75f69391150bd9b23324bb
r15hil <rishilapple@gmail.com>
2022-04-27 20:43:30 +0100

    Update allfreqblink.ino

--- Arduino/allfreqblink/allfreqblink.ino
+++ Arduino/allfreqblink/allfreqblink.ino
@@ -5,13 +5,22 @@ ezOutput led8(8);  // create ezOutput object that attach to pin 8;
 ezOutput led9(9);  // create ezOutput object that attach to pin 9;
 
 void setup() {
-  led7.blink(71, 71); // 500 milliseconds ON, 250 milliseconds OFF
-  led8.blink(50, 50); // 250 milliseconds ON, 250 milliseconds OFF
-  led9.blink(42, 42); // 100 milliseconds ON, 100 milliseconds OFF
+//  led7.blink(71, 71); 
+//  led8.blink(50, 50); 
+//  led9.blink(42, 42); 
+//    led7.blink(71, 71); 
+//    led8.blink(71, 71); 
+//    led9.blink(71, 71); 
+//    led7.blink(50, 50); 
+//    led8.blink(50, 50); 
+//    led9.blink(50, 50); 
+    led7.blink(42, 42); 
+    led8.blink(42, 42); 
+    led9.blink(42, 42);
 }
 
 void loop() {
-  led7.loop(); // MUST call the led1.loop() function in loop()
-  led8.loop(); // MUST call the led2.loop() function in loop()
-  led9.loop(); // MUST call the led3.loop() function in loop()
+  led7.loop(); 
+  led8.loop(); 
+  led9.loop();
 }
